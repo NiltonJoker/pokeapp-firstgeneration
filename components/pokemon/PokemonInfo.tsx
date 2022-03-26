1580169d4bb083de80496857247e94352f8611ac
Nilton <nilton242009@hotmail.com>
2022-03-26 04:42:02 -0500

    Save on Favorites Button Responsive

--- components/pokemon/PokemonInfo.tsx
+++ components/pokemon/PokemonInfo.tsx
@@ -12,7 +12,7 @@ export const PokemonInfo: FC<Props> = ({ pokemon }) => {
   return (
     <Grid xs={12} sm={8}>
       <Card>
-        <Card.Header css={{ display: "flex", justifyContent: "space-between" }}>
+        <Card.Header css={{ display: "flex", justifyContent: "space-between", flexWrap: 'wrap' }}>
           <Text h1 transform="capitalize">
             {pokemon.name}
           </Text>
